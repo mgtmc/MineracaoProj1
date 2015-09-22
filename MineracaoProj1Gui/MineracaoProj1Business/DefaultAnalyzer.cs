4b095a8e31e06f95f8c50064ee3d5edd267abfb0
Maria Gabriela <mgtmc@cin.ufpe.br>
2015-09-22 20:50:36 -0300

    sjdlgn

---
+++ MineracaoProj1Gui/MineracaoProj1Business/DefaultAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineracaoProj1Business
+{
+    public class DefaultAnalyzer : Analyzer
+    {
+        public override TokenStream TokenStream(string fieldName, TextReader reader)
+        {
+            TokenStream result = new LowerCaseTokenizer(reader);
+            result = new StandardFilter(result);
+            return result;
+        }
+    }
+}
